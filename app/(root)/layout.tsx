fdd0f6b6835e2cb339c3f9bd05bb4779873c85c0
Aditya Singh <jeevan56k@gmail.com>
2025-03-20 23:47:02 +0530

    Done with navbar and basic git auth

--- app/(root)/layout.tsx
+++ app/(root)/layout.tsx
@@ -2,9 +2,9 @@ import Navbar from "../components/Navbar";
 
 export default function Layout({children}: Readonly<{children:React.ReactNode}>){
     return(
-        <div>
+        <main className="font-work-sans">
             <Navbar/>
             {children}
-        </div>
+        </main>
     )
 }
--- app/(root)/page.tsx
+++ app/(root)/page.tsx
@@ -3,7 +3,8 @@ import Image from "next/image";
 export default function Home() {
   return (
    <>
-   <h1 className="text-2xl">Home </h1>
+   <h1 className="heading">Home </h1>
+   <h1>Home again</h1>
    </>
   );
 }
